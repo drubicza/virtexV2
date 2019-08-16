eeaef95fb4ef2bf456ff5f61b99fea736e3ce387
drubicza <id.slacker@gmail.com>
2019-08-16 15:31:43 +0800

    Add deobfuscated source

---
+++ v-deobfuscated.sh
@@ -0,0 +1,343 @@
+clear
+blue='\e[1;34m'
+green='\e[0;23m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+lgreen='\e[1;34m'
+brown='\e[0;33m'
+white='\e[1;37m'
+w='\e[97m'         #putih
+g='\033[1;92m'     #hijau
+r='\033[1;91m'     #merah
+b='\033[1;94m'     #biru
+uline='\e[1;4m'    #bergarisbawah
+c='\033[1;36m'     #cyan
+red='\033[1;31m'
+k='\033[1;33m'     #kuning
+u='\033[1;35m'     #ungu
+re='\033[0m'       #putih
+bg='\033[45;31m'
+bg2='\033[46;31m'
+bg3='\033[47;31m'
+echo
+echo $go" __      ___      _"
+echo $u" \ \    / (_)    | |    "$k"*"$c"HaxID"
+echo $r"  \ \  / / _ _ __| |_ _____  __"
+echo $b"   \ \/ / | | '__| __/ _ \ \/ /"
+echo $c"    \  /  | | |  | ||  __/>  <    "$k"*"$r"V2"
+echo $k"     \/   |_|_|   \__\___/_/(_)"
+echo $c"                              _    _    __"
+echo $r"[+]"$k" Author  :"$go"AsecC|~|eror404"$c" ( \/\/ )  /__*"
+echo $r"[+]"$k" Team    :"$go"HaxID          "$c"  )    (  /(__))"
+echo $r"[+]"$k" Contact :"$go"085218949365   "$c" (__/\__)(__)(__)"
+echo
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $b " [*] (Ctrl + C ) Detected, Trying To Exit ... "
+echo $g " [*] Thanks"
+sleep 1
+echo $c " [*] Semoga Bermanfaat "
+echo $k " [*] By AsecC eror404 :)..."
+sleep 1
+exit
+}
+lagi=1
+while [ $lagi -lt 14 ];
+do
+echo $re
+echo $k "["$r"21"$k"] "$c"Virtex Mr.ZeroX"
+echo $k "["$r"22"$k"] "$c"V̺͆I̺͆R̺͆T̺͆E̺͆X̺͆T̺͆ B̺͆Y̺͆ G̺͆H̺͆O̺͆S̺͆T̺͆N̺͆A̺͆M̺͆E"
+echo $k "["$r"23"$k"] "$c"VIRUS ☥⚛☭ɱɼ¬Ƙ͋Ơ̻ƙ̈́Ʋ᷵ᷥᷤᷫᷫᷪ⚛☥"
+echo $k "["$r"24"$k"] "$c"Virus by TnT.404"
+echo $k "["$r"25"$k"] "$c"VIRUS LORD CHOKY"
+echo $k "["$r"26"$k"] "$c"Virus MASTER ERORRR GANAS!!"
+echo $k "["$r"27"$k"] "$c"VIRUS"
+echo $k "["$r"28"$k"] "$c"VIRUS2"
+echo $k "["$r"29"$k"] "$c"VIRTEX SAYANG"
+echo $k "["$r"30"$k"] "$c"VIRTEX SABAR"
+echo $k "["$r"31"$k"] "$c"VIRTEX PERTANYAAN"
+echo $k "["$r"32"$k"] "$c"VIRTEX PATAH HATI"
+echo $k "["$r"33"$k"] "$c"VIRTEX MENJELASKAN"
+echo $k "["$r"34"$k"] "$c"VIRTEX LOVE"
+echo $k "["$r"35"$k"] "$c"VIRTEXT BARA API"
+echo $k "["$r"36"$k"] "$c"VIRTEX BULATAN HITAM"
+echo $k "["$r"37"$k"] "$c"VIRTEX BOM"
+echo $k "["$r"38"$k"] "$c"Đ.Δ.Μ.Ň.į.Ҝ.Δ.ᇸ"
+echo $k "["$r"39"$k"] "$c"VIRTEX NOMOR"
+echo $k "["$r"40"$k"] "$c"SERIGALA HITAM"
+echo
+echo $k "["$r"00"$k"] "$k"Prev Page"
+echo
+echo $c
+read -p "[+] Enter Choice : " pil;
+case $pil in
+21) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex21.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V21.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+22) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex22.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V22.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+23) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex23.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V23.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+24) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex24.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V24.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+25) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex25.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V25.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+26) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex26.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V26.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+27) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex27.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V27.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+28) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex28.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V28.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+29) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex29.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V29.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+30) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex30.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V30.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+31) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex31.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V31.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+32) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex32.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V32.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+33) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex33.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V33.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+34) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex34.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V34.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+35) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex35.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V35.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+36) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex36.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V36.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+37) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex37.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V37.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+38) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex38.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V38.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+39) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex39.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V39.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+40) cd $HOME/storage/shared/virtex
+figlet Sabar.. | lolcat
+sleep 2
+echo $c"Loading..."
+sleep 2
+echo $c"Sedang di proses.."
+sleep 2
+curl -o virtex40.txt https://raw.githubusercontent.com/muhammadfathul/vir/master/V40.txt
+echo $k"Virtex Tersimpan di Penyimpanan internal folder virtex"
+sleep 2
+figlet Selesai | lolcat
+sleep 3
+;;
+00) exit
+;;
+*) echo $r "[!] Pilihan Yang anda masukan tidak tersedia!!! "
+sleep 2
+esac
+done
+done
